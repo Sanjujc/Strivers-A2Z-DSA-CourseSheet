97b91865fa4f1b2c6869025ec4d113e8d238e790
Sanjujc <sanjujayachandran3458@gmail.com>
2024-10-20 23:02:53 +0530

    remove duplicates

--- array_easy/remove_duplicate.py
+++ array_easy/remove_duplicate.py
@@ -1,5 +1,15 @@
 def remove_duplicate(arr):
-    return list(set(arr))
+    i = 0
+    j = 0
+    n = len(arr)
+    for each in range(n):
+        if arr[i] != arr[j]:
+            arr[i+1] = arr[j]
+            j = j+1
+            i = i+1
+        else:
+            j = j+1
 
+    return i+1
 
-print(remove_duplicate([1,2,2,3,4,5,3]))
+print(remove_duplicate([1,1,2]))
