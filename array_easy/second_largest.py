c1254188cff258fcc5d622eac85fb93efd09a7a7
Sanjujc <sanjujayachandran3458@gmail.com>
2024-10-20 23:03:38 +0530

    second largest

--- array_easy/second_largest.py
+++ array_easy/second_largest.py
@@ -10,9 +10,8 @@ def second_largest(arr: List[int]) -> int:
         if arr[each] > large_number:
             second_largest_number = large_number
             large_number = arr[each]
-
-        elif large_number > arr[each]  > second_largest_number:
+        elif large_number > arr[each] > second_largest_number:
             second_largest_number =arr[each]
     return second_largest_number
 
-print(second_largest([1,2,8,4,6,5]))
+print(second_largest([1,2,8,4,7,6,5]))
