302be0538e09b178d0bd85a2ebc49c81b182411c
Sanju Jayachandran <88083188+Sanjujc@users.noreply.github.com>
2024-10-28 22:51:09 +0530

    shift one place

--- array_easy/rotate_n_place.py
+++ array_easy/rotate_n_place.py
@@ -1,15 +1,11 @@
 def rotate_n_place(arr, place):
-    temp = arr[:len(arr)-place]
+    temp = arr[:place]
     print(temp)
-    i = 0
-    for each in range(place, len(arr)):
-        arr[each - place] = arr[each]
-        i += 1
-    print(arr)
-    for each in range(0, len(temp)):
-        arr[i] = temp[each]
-        i = i + 1
-    print(arr)
+    if len(arr) ==0:
+        return None
+    for each in range(len(arr)-1):
+        arr[each] = arr[each+1]
+    arr[-1] = temp[0]
+    return arr
+print(rotate_n_place([1, 2, 3, 4, 5], 1))
 
-
-print(rotate_n_place([1, 2, 3, 4, 5], 4))
