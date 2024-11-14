aa2e251abee4ee7c63599603d8f3fb8c6d7ed399
Sanju Jayachandran <88083188+Sanjujc@users.noreply.github.com>
2024-11-15 00:20:03 +0530

    two sum

--- BlindSheet/Array/twoSum.py
+++ BlindSheet/Array/twoSum.py
@@ -1,4 +1,4 @@
-a = [2,6,5,8,11]
+a = [2,6,8,5,11]
 target = 14
 
 hash_map  = {
@@ -7,4 +7,24 @@ hash_map  = {
 
 for each in range(0,len(a)):
     diff = target - a[each]
-    if diff not in
+    print(diff)
+    if diff not in hash_map:
+        hash_map[a[each]] = each
+    else:
+        print(hash_map[diff],each)
+        print(hash_map)
+        break
+
+sorted(a)
+i = 0
+j = len(a) -1
+while i < j:
+    if a[i] + a[j] == target:
+        print('Yes')
+        break
+    elif a[i] + a[j] > target:
+        j -= 1  # Move the right pointer left
+    else:
+        i += 1  # Move the left pointer right
+else:
+    print('No')
