e2022a9bdd96e634954da5fa8badac28d18ad506
Sanju Jayachandran <88083188+Sanjujc@users.noreply.github.com>
2024-11-17 11:10:25 +0530

    Python code updated

--- Basic_Maths/Count Digits.py
+++ Basic_Maths/Count Digits.py
@@ -1,16 +1,7 @@
-def digit_count(n):
-    n = abs(n)  # Use abs to handle negative numbers
-    n_str = str(n)
-    count = 0
-    for x in n_str:
-        digit = int(x)  # Convert character back to an integer
-        if digit != 0 and n % digit == 0:  # Check if the digit is not zero and is a divisor
-            count += 1
-
-    return count
-
-
-# Example usage
-number = 2446
-count = digit_count(number)
-print(f"The number of digits in {number} is: {count}")
+n = 1234
+counter = 0
+while n > 0:
+	digit = n % 10
+	counter = counter + 1
+	n = n //10
+print(counter)
--- HashMap/hash_map.py
+++ HashMap/hash_map.py
@@ -1,13 +1,16 @@
-def frequencyCount(arr, N, P):
-    hash_arr = [0] * int(N + 1)
-    print(hash_arr)
-    for each in arr:
-        if hash_arr[each] == 0:
-            hash_arr[each] =1
-        else:
-            hash_arr[each] +=1
-    return hash_arr
-n = 5
-arr = [2, 3, 2, 3, 5]
-p = 5
-print(frequencyCount(arr,n,p))
+a = [2,4,5,6,7]
+target = 9
+lower =  0
+upper =len(a)-1
+while lower < upper:
+	mid =  (lower+upper) //2
+	if target == a[mid]:
+		print('Element found at position',mid)
+		break
+	elif target > a[mid]:
+		lower = mid + 1  # Move the lower bound up since the target is greater than mid element
+	else:
+		upper = mid - 1
+else:
+	print('Element not found')
+
