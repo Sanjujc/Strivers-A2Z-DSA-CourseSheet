ba866786f83372a92de00d26240b00b7a1a122ce
Sanju Jayachandran <88083188+Sanjujc@users.noreply.github.com>
2024-11-25 20:22:29 +0530

    insertion_sorting

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,11 @@
+a= [9,8,7,6,5,4,4,3,1]
+n = len(a)
+
+for i in range(1,n):
+    j = i - 1
+    key = a[i]
+    while j >-1 and a[j] > key:
+        a[j+1] = a[j]
+        j -=1
+    a[j+1] = key
+print(a)
